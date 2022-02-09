d3dfea0b5fdf20943a1dec39d0d6ee67dcc24a91
Mahmoud Amr <engineer.mahmoudamr@gmail.com>
2022-02-09 02:32:04 +0200

    Add project files.

---
+++ AnimalMatcher/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AnimalMatcher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetUpGame();
+        }
+
+        private void SetUpGame()
+        {
+
+            List<string> animalEmoji = new List<string>()
+            {
+                "🐭","🐭",
+                "🐶","🐶",
+                "🐯","🐯",
+                "🦒","🦒",
+                "🐱","🐱",
+                "🐺","🐺",
+                "🐗","🐗",
+                "🦁","🦁",
+            };
+
+            Random rand = new Random();
+
+            foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+            {
+                
+
+
+            }
+        }
+    }
+}
